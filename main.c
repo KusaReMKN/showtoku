d45ac3ab37f9be4ec486c9c9014f8a50040f702e
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2024-05-08 19:55:37 +0900

    [WIP] -s -d

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 #define MAX(a, b)	((a) > (b) ? (a) : (b))
 
 static void	usage(void);
+static void	dump_reader(int fd, const char *path, int jammed);
 static void	simple_reader(int fd, const char *path, int jammed);
 
 int
@@ -26,8 +27,14 @@ main(int argc, char *argv[])
 	void (*reader)(int, const char *, int);
 
 	reader = simple_reader;
-	while ((c = getopt(argc, argv, "")) != -1)
+	while ((c = getopt(argc, argv, "ds")) != -1)
 		switch (c) {
+		case 's':
+			reader = simple_reader;
+			break;
+		case 'd':
+			reader = dump_reader;
+			break;
 		case '?':
 		default:
 			usage();
@@ -56,8 +63,7 @@ main(int argc, char *argv[])
 	}
 
 	while (/* CONSTCOND */ 1) {
-		rfds = fds;
-		//(void)memcpy(&rfds, &fds, sizeof(rfds));
+		(void)memcpy(&rfds, &fds, sizeof(rfds));
 		if ((ready = select(nfds, &rfds, NULL, NULL, NULL)) == -1)
 			err(EXIT_FAILURE, "select");
 		for (i = 0; i < nfds; i++)
@@ -66,6 +72,22 @@ main(int argc, char *argv[])
 	}
 
 	return -1;
+}
+
+static void
+dump_reader(int fd, const char *path, int jammed)
+{
+	ssize_t nread;
+	char buf[READSIZE];
+	int i;
+
+	if ((nread = read(fd, buf, sizeof(buf))) == -1)
+		err(EXIT_FAILURE, "%s", path);
+
+	(void)printf("%s%s (%zd):\t", jammed ? "[JAM]" : "", path, nread);
+	for (i = 0; i < nread; i++)
+		printf("%02X ", buf[i] & 0xFF);
+	putchar('\n');
 }
 
 static void
